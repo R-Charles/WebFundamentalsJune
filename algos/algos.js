15b0964b6ad1ca71897b10cc66b13c7621138dbf
R-Charles <Rapcharlo@aol.com>
2022-06-24 15:54:22 -0700

    user card update

--- algos/algos.js
+++ algos/algos.js
@@ -1,4 +1,27 @@
-sum = 0;
-let (x=0; x<10; x++);
-sum=x+1
-console.log(sum)
+// function greetUser(firstName, lastName){
+//     console.log("Hello " + firstName + "" + lastName);
+// }
+
+// greetUser("Thiery", "Nis")
+// greetUser("Jennifer", "Lopez")
+// greetUser("Inspector", "Gadget")
+
+// THE VALUE OF A FUNCTION CALL IS WHATEVER THAT FUNCTION CALL RETURNS
+// function sum(num1,num2){
+//     console.log("before the return")
+//     return num1 + num2
+// }
+
+// let someNum = sum(3,9) + sum(1,2)
+
+// console.log(someNum)
+
+function searchForArtist(artistName){
+    for(let i=0; i<artists.length; i++){
+        if(artists[i] == artistName){
+            return "Found" + artistName + "at index number" + i
+        }
+            
+    }
+    return "No artist foind with that name."
+    }
