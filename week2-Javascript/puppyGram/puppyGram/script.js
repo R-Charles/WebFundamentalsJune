6ef9f13c0eab181ad0fad95ed7d8dd5ab16522aa
R-Charles <Rapcharlo@aol.com>
2022-06-27 17:57:27 -0700

    weekend updates

---
+++ week2-Javascript/puppyGram/puppyGram/script.js
@@ -0,0 +1,14 @@
+function removeElement(element){
+    console.log("trying to remove something...wazaaa");
+    element.remove();
+}
+
+function playVideo(element){
+    console.log("wazaa playing video!");
+    element.play();
+}
+
+
+function changeImg(element){
+    console.log("wazaaaa", element);
+}
