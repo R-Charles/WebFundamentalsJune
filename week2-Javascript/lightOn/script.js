a4505bb9a4517d4ad99ffb802f79d462da2b8f2a
R-Charles <Rapcharlo@aol.com>
2022-07-05 14:55:32 -0700

    python add

---
+++ week2-Javascript/lightOn/script.js
@@ -0,0 +1,23 @@
+function changeImg(element){
+    element.src = "./Asset 4.png";
+
+ } 
+ else { if(element.src = "./Asset 4.png") {
+    element.src = "./Asset 2.png";
+ }
+
+ } 
+
+//  function changeOver(element){
+//     if (element.src = "./Asset 4.png"){
+//         element.src = "./Asset 2.png";
+//     }
+        
+// }
+ 
+//  function changeImage2(element){
+//         if (element.src = "./Asset 4.png"){
+//             element.src = "./Asset 2.png";
+//         }
+
+// }
---
+++ week2-Javascript/weatherFor/script.js
@@ -0,0 +1,5 @@
+function cookieMonster(element){
+    console.log("cookie monster");
+    var element = document.querySelector("#cookie-box");
+    element.remove();
+}
